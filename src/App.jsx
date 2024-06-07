3a8a816e9f499255b2d5120876e038ae990d4a4f
Carolina Bello <carolina.abello@gmail.com>
2024-06-07 14:16:07 +0000

    Implementação das propriedades do componente <Card />

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
   return (
     <>
       <div className="cards">
-        <Card />
-        <Card />
-        </div>
-      </>
+        <Card item={item1} />
+        <Card item={item2} />
+      </div>
+    </>
   )
 }
 
